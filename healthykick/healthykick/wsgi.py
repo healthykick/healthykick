1f5e9211b162b91264be5ee55edfd3c297e12492
Aneep Tandel <aneepct@live.com>
2016-06-07 11:37:02 +0530

    Update gitignore file

--- healthykick/healthykick/wsgi.py
+++ healthykick/healthykick/wsgi.py
@@ -1,16 +1,19 @@
 """
-WSGI config for healthykick project.
+WSGI config for tandel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/var/www/django/healthykick')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'healthykick.settings'
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthykick.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tandel.settings")
 
 application = get_wsgi_application()
